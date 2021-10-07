216cf551073f9fb3310383e1bb3c7e52cca615bb
c-carrillo <78883575+c-carrillo@users.noreply.github.com>
2021-10-06 21:05:55 -0700

    Fixed error with route again

--- squad-seek/src/App.js
+++ squad-seek/src/App.js
@@ -1,11 +1,45 @@
+//Import of different pages
+import RegisterUser from "./pages/RegisterUser";
+import MainHeader from "./Components/MainHeader/MainHeader";
+import Groups from "./pages/Groups";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+//import styling
+import "./App.css"
+//import of router to go to different pages
+import { Redirect } from 'react-router';
+import {Route, Router, Link} from "react-router-dom";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Squad Seek</h1>
+    <div className="bg-dark fluid vh-100"> 
+      
+      {/* Call to the top navigation */}
+      <MainHeader />
+
+      <main>
+      <Redirect from="/" to="/welcome" />
+
+      {/* route to the welcome page and call welcome component */}
+      <Route exact path="/welcome">
+          <Welcome />
+        </Route>
+        {/* route to the register page and call welcome component */}
+        <Route path="/register">
+          <RegisterUser />
+        </Route>
+        {/* route to the groups page and call welcome component */}
+        <Route path="/groups">
+          <Groups />
+        </Route>
+        {/* route to the login page and call welcome component */}
+        <Route path="/login">
+          <Login />
+        </Route>
+      </main>
+      
     </div>
   );
-}
+};
 
 export default App;
