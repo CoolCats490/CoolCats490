1f72f28caa48a79c4b26909be2fe24a936790fa8
SinghGurpreet1996 <singh.gurpreet5683@gmail.com>
2021-10-14 11:45:40 -0700

    Server and Database connected

--- squad-seek/src/pages/Welcome.js
+++
@@ -1,9 +0,0 @@
-import { Container } from "react-bootstrap";
-
-const Welcome = () =>{
-    return <Container fluid className="text-white d-flex justify-content-center" >
-        <h1>Home Page</h1>
-    </Container>
-};
-
-export default Welcome;
