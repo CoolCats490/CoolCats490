8e0ec2d61e7b06ea40b342ce7452367d7face33b
c-carrillo <78883575+c-carrillo@users.noreply.github.com>
2021-11-29 02:12:51 -0800

    Made the group details auto refresh

--- squad-seek/src/pages/GroupDetails.js
+++ squad-seek/src/pages/GroupDetails.js
@@ -17,7 +17,6 @@ import {
   Modal,
   ListGroup,
   Accordion,
-  Card
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 //pic
@@ -50,51 +49,52 @@ const GroupDetails = (props) => {
   const [userInfo, setUserInfo] = useState([]);
 
   //U
-  const [dataChanged, setDataChanged] = useState(false)
+  const [dataChanged, setDataChanged] = useState(false);
   ///////////////////////////////////////////////////////////////
-  const loadData = useCallback( async () =>{
+
+  const loadData = useCallback(async () => {
     //async call to database
-      const fetchGroups = async () => {
-        try {
-          let response = await axios(
-            `http://localhost:5000/activities/${params.groupID}`
-          );
-          //store groups in groups object
-          setGroups(response.data);
-          setLoading(false);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-  
-      let fetchUser = async () =>{
-        try{
-          const response = await axios.get
-        ("http://localhost:5000/users/me", {
+    const fetchGroups = async () => {
+      try {
+        let response = await axios(
+          `http://localhost:5000/activities/${params.groupID}`
+        );
+        //store groups in groups object
+        setGroups(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    let fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/users/me", {
           headers: {
             "Content-Type": "application/json",
             token: authCtx.token,
           },
         });
         //store user info in user object
-        setUserInfo(response.data)
-  
-        }catch(err){
-          console.log(err);
-        }
+        setUserInfo(response.data);
+      } catch (err) {
+        console.log(err);
       }
-      //Call async function
-      fetchGroups();
-      if(isLogedIn)
-        fetchUser();
+    };
+    //Call async function
+    fetchGroups();
+    if (isLogedIn) fetchUser();
     //set datachange back to false
-      setDataChanged(false)
-  },[dataChanged,params,authCtx.token,isLogedIn])
-
+    //setDataChanged(false);
+    console.log("useCallback called")
+    //setDataChanged(dataChanged + 1)
+  }, [params, authCtx.token, isLogedIn, dataChanged]);
 
   //useEffect hook will load groups from data base when component is loaded
   useEffect(() => {
-    loadData()
+    loadData();
+    setDataChanged(false);
+    console.log("useEffect called")
   }, [loadData]);
 
   //if data is not loaded will retrun a blank page saying loading
@@ -102,39 +102,44 @@ const GroupDetails = (props) => {
     return <Container>Data is Loading</Container>;
   }
 
+ 
   const joinBtnHandler = (event) => {
+    let alreadyJoined = groups.members.find((x) => x.id === userInfo._id);
 
-    let alreadyJoined = groups.members.find(x => x.id === userInfo._id)
-
-    if(!alreadyJoined){
-      console.log("not in group")
+    if (!alreadyJoined) {
+      console.log("not in group");
 
       //get user stuff
       let memberInfo = {
-        id:userInfo._id,
-        username:userInfo.username
-      }
+        id: userInfo._id,
+        username: userInfo.username,
+      };
 
-      groups.members.push(memberInfo)
+      groups.members.push(memberInfo);
 
       const groupStuff = {
-        members: groups.members
-      }
-  
-      try {//http://localhost:5000/activities/join/:id
-        axios.post('http://localhost:5000/activities/join/'+groups._id, groupStuff).then(res=> console.log(res.data));
+        members: groups.members,
+      };
+
+      try {
+        //http://localhost:5000/activities/join/:id
+        axios
+          .post(
+            "http://localhost:5000/activities/join/" + groups._id,
+            groupStuff
+          )
+          .then((res) => console.log(res.data));
       } catch (err) {
-            console.log(err);
+        console.log(err);
       }
 
       //Update the page data again
-      setDataChanged(true)
-
+      setDataChanged(true);
     }
-
   };
+  
 
-  //Date stuff
+  //Formatting date into a readable format
   let date = new Date(parseInt(groups.time));
   let month = date.toLocaleString("en-US", { month: "long" });
   let day = date.toLocaleString("en-US", { day: "2-digit" });
@@ -162,9 +167,12 @@ const GroupDetails = (props) => {
               <h2>
                 <strong>Group Title:</strong> {groups.name}
               </h2>
-              {groups.createdBy !=null &&(<p>
-                <strong>Group Started by:</strong> {groups.createdBy[0].username}
-              </p>)}
+              {groups.createdBy != null && (
+                <p>
+                  <strong>Group Started by:</strong>{" "}
+                  {groups.createdBy[0].username}
+                </p>
+              )}
               <p>
                 <strong>Group Type:</strong>{" "}
                 {parseInt(groups.type) ? "Online" : "In Person"}{" "}
@@ -184,51 +192,54 @@ const GroupDetails = (props) => {
                   </Badge>
                 ))}
               </p>
-              {groups.createdBy[0].id === userInfo._id && (<Button onClick={() => setShowUpdateModal(true)} className="pr-2">Update</Button>)}
-              {isLogedIn && groups.createdBy[0].id === userInfo._id && (<Button variant="danger" onClick={() => setShowDeleteModal(true)}>
-                Delete
-              </Button>)}
-              {isLogedIn && groups.createdBy[0].id !== userInfo._id && (<Button onClick={joinBtnHandler}>Join Group</Button>)}
+              {groups.createdBy[0].id === userInfo._id && (
+                <Button
+                  onClick={() => setShowUpdateModal(true)}
+                  className="pr-2"
+                >
+                  Update
+                </Button>
+              )}
+              {isLogedIn && groups.createdBy[0].id === userInfo._id && (
+                <Button
+                  variant="danger"
+                  onClick={() => setShowDeleteModal(true)}
+                >
+                  Delete
+                </Button>
+              )}
+              {isLogedIn && groups.createdBy[0].id !== userInfo._id 
+              && !groups.members.find( ({id}) => id === userInfo._id) && (
+                <Button onClick={joinBtnHandler}>Join Group</Button>
+              )}
             </section>
           </Col>
         </Row>
         <Row>
-        <Container fluid >
-        {groups.members && (<h4 className="pt-4"></h4>)}
-        {/* <ListGroup className="pb-4">
-          {
-              groups.members && 
-              groups.members.map((e,index) =>(
-                <ListGroup.Item key={e._id} key={index}>{e.username}</ListGroup.Item>
-              )
-              )
-          }
+          <Container fluid>
+            {groups.members && <h4 className="pt-4"> </h4>}
 
-        </ListGroup> */}
-        <Accordion className="text-dark pb-4 ">
-                        <Card>
-                            <Accordion.Toggle as={Card.Header} eventKey="0">
-                            <h4 className="pt-4 text-center">Click to see members in group</h4>
-                            </Accordion.Toggle>
-
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body>
-                                <ListGroup className="pb-4">
-          {
-              groups.members && 
-              groups.members.map((e,index) =>(
-                <ListGroup.Item key={e._id} key={index}>{e.username}</ListGroup.Item>
-              )
-              )
-          }
-
-        </ListGroup>
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                    </Accordion>
-
-        </Container>
+            <Accordion className="text-black pb-4">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                  Members in group <pre> </pre>
+                  {groups.members && (
+                    <Badge bg="info">{groups.members.length}</Badge>
+                  )}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <ListGroup className="pb-4">
+                    {groups.members &&
+                      groups.members.map((e, index) => (
+                        <ListGroup.Item key={e._id} key={index}>
+                          {e.username}
+                        </ListGroup.Item>
+                      ))}
+                  </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Container>
         </Row>
       </Container>
 
