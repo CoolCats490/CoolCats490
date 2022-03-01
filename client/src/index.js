b4415d81e28c6c34d8a9b6824d53a78b6478a8af
c-carrillo <78883575+c-carrillo@users.noreply.github.com>
2022-03-01 12:10:22 -0800

    Fixed bug that made pages not to refresh on azure

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./Store/auth-context";
 
 ReactDOM.render(
   <React.StrictMode>
   <AuthContextProvider>
     {/* BrowserRouter is used to route to different pages of the site */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
